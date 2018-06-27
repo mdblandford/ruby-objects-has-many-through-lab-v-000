973f0ad82c923a154dbb8731761c71dd9857839a
Michael Blandford <mdblandford@gmail.com>
2018-06-27 14:40:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,16 @@
 class Song 
   attr_accessor :name, :genre, :artist
-  
-  def initialize(name, genre)
+  @@all = []
+
+  def initialize(name, genre, artist)
     @name = name
-    @genre = genre 
-    genre.add_song(self) 
+    @genre = genre
+    @artist = artist
+    @@all << self
   end
+  #def initialize(name, genre)
+  #  @name = name
+  #  @genre = genre 
+  #  genre.add_song(self) 
+  #end
 end
